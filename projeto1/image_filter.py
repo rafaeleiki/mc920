ddff7ee10b46a0ca64fbf5c80c57d289942ec30a
Rafael Eiki <rafaeleiki@yahoo.com.br>
2019-04-15 16:34:11 -0300

    Improved code documentation

--- projeto1/image_filter.py
+++ projeto1/image_filter.py
@@ -6,6 +6,12 @@ class ImageFilter:
 
     @staticmethod
     def filter(matrix, input_file, output_file):
+        """
+        Cria uma imagem, usando a matriz como base para o filtro.
+        :param matrix: matriz a ser usada para o filtro
+        :param input_file: arquivo de entrada
+        :param output_file: arquivo de saída
+        """
         img_file = cv2.imread(input_file)
         new_img = cv2.filter2D(img_file, -1, matrix)
         cv2.imwrite(output_file, new_img)
@@ -13,6 +19,11 @@ class ImageFilter:
 
     @staticmethod
     def filter_a(input_file, output_file):
+        """
+        Filtra uma imagem
+        :param input_file: arquivo de entrada
+        :param output_file: arquivo de saída
+        """
         matrix = [[0,  0, -1,  0, 0],
                   [0, -1, -2, -1, 0],
                   [-1, -2, 16, -2, -1],
@@ -23,6 +34,11 @@ class ImageFilter:
 
     @staticmethod
     def filter_b(input_file, output_file):
+        """
+        Filtra uma imagem
+        :param input_file: arquivo de entrada
+        :param output_file: arquivo de saída
+        """
         matrix = [[1,  4, 6,  4, 1],
                   [4, 16, 24, 16, 4],
                   [6, 24, 36, 24, 6],
@@ -33,6 +49,11 @@ class ImageFilter:
 
     @staticmethod
     def filter_c(input_file, output_file):
+        """
+        Filtra uma imagem
+        :param input_file: arquivo de entrada
+        :param output_file: arquivo de saída
+        """
         matrix = [[-1, 0, 1],
                   [-2, 0, 2],
                   [-1, 0, 1]]
@@ -41,6 +62,11 @@ class ImageFilter:
 
     @staticmethod
     def filter_d(input_file, output_file):
+        """
+        Filtra uma imagem
+        :param input_file: arquivo de entrada
+        :param output_file: arquivo de saída
+        """
         matrix = [[-1, -2, -1],
                   [0, 0, 0],
                   [1, 2, 1]]
@@ -49,6 +75,11 @@ class ImageFilter:
 
     @staticmethod
     def filter_c_d(file_1, file_2, output_file):
+        """
+        Filtra uma imagem
+        :param input_file: arquivo de entrada
+        :param output_file: arquivo de saída
+        """
         img_file_c = np.float32(cv2.imread(file_1))
         img_file_d = np.float32(cv2.imread(file_2))
         square = np.square(img_file_c) + np.square(img_file_d)
@@ -57,4 +88,8 @@ class ImageFilter:
 
     @staticmethod
     def __log_file_creation(file):
+        """
+        Realiza o log da criação dos arquivos de imagem filtrados
+        :param file: nome do arquivo que foi criado
+        """
         print(f"Arquivo '{file}' escrito")
--- projeto1/main.py
+++ projeto1/main.py
@@ -2,7 +2,10 @@ import os
 from projeto1.image_filter import ImageFilter
 
 
+# Gera todas as versões da imagem com cada filtro
 def generate_filtered_images(file_path):
+
+    # Descobre o nome do arquivo e a extensão
     filename = os.path.basename(file_path)
     filename_parts = os.path.splitext(filename)
     base = "".join(filename_parts[0:-1])
@@ -11,6 +14,7 @@ def generate_filtered_images(file_path):
     c_filter_file = f"./results/{base}_c{extension}"
     d_filter_file = f"./results/{base}_d{extension}"
 
+    # Gera as imagens com cada um dos filtros
     ImageFilter.filter_a(file_path, f"./results/{base}_a{extension}")
     ImageFilter.filter_b(file_path, f"./results/{base}_b{extension}")
     ImageFilter.filter_c(file_path, c_filter_file)
@@ -18,6 +22,7 @@ def generate_filtered_images(file_path):
     ImageFilter.filter_c_d(c_filter_file, d_filter_file, f"./results/{base}_c_d{extension}")
 
 
+# Cria imagens filtradas a partir dos arquivos de imagens dados
 def main():
     generate_filtered_images("../pictures/baboon.png")
     generate_filtered_images("../pictures/butterfly.png")
