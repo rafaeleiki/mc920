fa4ad03d4b7ded5c055cf55ad0b68bc30f5d9395
Rafael Eiki <rafaeleiki@yahoo.com.br>
2019-04-17 16:59:22 -0300

    Finished project 1

--- projeto1/image_filter.py
+++ projeto1/image_filter.py
@@ -7,19 +7,43 @@ class ImageFilter:
     @staticmethod
     def __normalize_image(image):
         """
-        Normaliza e escreve uma imagem
+        Normaliza uma imagem no intervalo de 0 a 255
         :param image: imagem a ser desenhada no arquivo
         """
         new_image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX)
         return new_image
 
     @staticmethod
-    def filter_h1(image):
+    def __binary_normalization_image(image):
         """
-        Filtra uma imagem
+        Transforma a imagem em binária. Qualquer valor abaixo de 128 é transformado em 0; caso contrário, em 255.
+        :param image: imagem a ser transformada
+        :return: imagem com intensidades exclusivas de 0 e 255
+        """
+        ret, new_img = cv2.threshold(image, 128, 255, cv2.THRESH_BINARY)
+        return new_img
+
+    @staticmethod
+    def __normalize_image_binary_option(image, binary):
+        """
+        Normaliza uma imagem levando em consideração se vai ser binária
+        :param image: imagem a ser transformada
+        :param binary: indica se a imagem tem que ser em preto e branco
+        :return: imagem transformada
+        """
+        if binary:
+            new_image = ImageFilter.__binary_normalization_image(image)
+        else:
+            new_image = ImageFilter.__normalize_image(image)
+        return new_image
+
+    @staticmethod
+    def filter_h1(image, binary=True):
+        """
+        Filtra uma imagem com um filtro passa-alta
         :param image: imagem a ser filtrada
-        :param normalize: indica se a imagem deve ser normalizada. Por padrão, ela é normalizada.
-        :return: imagem filtrada, podendo estar normalizada no intervalo de 0 a 255
+        :param binary: indica se a imagem deve ser em preto e branco
+        :return: imagem filtrada, normalizada no intervalo de 0 a 255
         """
         matrix = [[0,  0, -1,  0, 0],
                   [0, -1, -2, -1, 0],
@@ -28,15 +52,15 @@ class ImageFilter:
                   [0,  0, -1,  0, 0]]
         matrix = np.array(matrix)
         new_image = cv2.filter2D(image, -1, matrix)
-        return ImageFilter.__normalize_image(new_image)
+        return ImageFilter.__normalize_image_binary_option(new_image, binary)
 
     @staticmethod
-    def filter_h2(image):
+    def filter_h2(image, binary=False):
         """
-        Filtra uma imagem
+        Filtra uma imagem com um filtro passa-baixa
         :param image: imagem a ser filtrada
-        :param normalize: indica se a imagem deve ser normalizada. Por padrão, ela é normalizada.
-        :return: imagem filtrada, podendo estar normalizada no intervalo de 0 a 255
+        :param binary: indica se a imagem deve ser em preto e branco
+        :return: imagem filtrada, normalizada no intervalo de 0 a 255
         """
         matrix = [[1,  4, 6,  4, 1],
                   [4, 16, 24, 16, 4],
@@ -45,53 +69,59 @@ class ImageFilter:
                   [1,  4, 6,  4, 1]]
         matrix = np.array(matrix) / 256
         new_image = cv2.filter2D(image, -1, matrix)
-        return ImageFilter.__normalize_image(new_image)
+        return ImageFilter.__normalize_image_binary_option(new_image, binary)
 
     @staticmethod
-    def filter_h3(image):
+    def filter_h3(image, binary=True):
         """
-        Filtra uma imagem
+        Filtra uma imagem com um filtro passa-alta que mantém os traços verticais
         :param image: imagem a ser filtrada
-        :param normalize: indica se a imagem deve ser normalizada. Por padrão, ela é normalizada.
-        :return: imagem filtrada, podendo estar normalizada no intervalo de 0 a 255
+        :param binary: indica se a imagem deve ser em preto e branco
+        :return: imagem filtrada, normalizada no intervalo de 0 a 255
         """
         matrix = [[-1, 0, 1],
                   [-2, 0, 2],
                   [-1, 0, 1]]
         matrix = np.array(matrix)
         new_image = cv2.filter2D(image, -1, matrix)
-        return ImageFilter.__normalize_image(new_image)
+        return ImageFilter.__normalize_image_binary_option(new_image, binary)
 
     @staticmethod
-    def filter_h4(image):
+    def filter_h4(image, binary=True):
         """
-        Filtra uma imagem
+        Filtra uma imagem com um filtro passa-alta que mantém os traços horizontais
         :param image: imagem a ser filtrada
-        :param normalize: indica se a imagem deve ser normalizada. Por padrão, ela é normalizada.
-        :return: imagem filtrada, podendo estar normalizada no intervalo de 0 a 255
+        :param binary: indica se a imagem deve ser em preto e branco
+        :return: imagem filtrada, normalizada no intervalo de 0 a 255
         """
         matrix = [[-1, -2, -1],
                   [0, 0, 0],
                   [1, 2, 1]]
         matrix = np.array(matrix)
         new_image = cv2.filter2D(image, -1, matrix)
-        return ImageFilter.__normalize_image(new_image)
+        return ImageFilter.__normalize_image_binary_option(new_image, binary)
 
     @staticmethod
-    def filter_h3_h4(image):
+    def filter_h3_h4(image, binary=True):
         """
-        Filtra uma imagem
+        Filtra uma imagem com um filtro passa-alta, combinando traços horizontais e verticais
         :param image: imagem a ser filtrada
-        :param normalize: indica se a imagem deve ser normalizada. Por padrão, ela é normalizada.
-        :return: imagem filtrada, podendo estar normalizada no intervalo de 0 a 255
+        :param binary: indica se a imagem deve ser em preto e branco
+        :return: imagem filtrada, normalizada no intervalo de 0 a 255
         """
         image_1 = ImageFilter.filter_h3(image)
         image_2 = ImageFilter.filter_h4(image)
         new_image = np.hypot(image_1, image_2)
-        return ImageFilter.__normalize_image(new_image)
+        return ImageFilter.__normalize_image_binary_option(new_image, binary)
 
     @staticmethod
     def filter_gaussian(image, sigma):
+        """
+        Filtra uma imagem com um filtro Gaussiano no domínio da frequência
+        :param image: imagem a ser filtrada
+        :param sigma: desvio padrão do filtro Gaussiano
+        :return: imagem filtrada
+        """
         # Calcula a transformada e translada a faixa-zero para o centro
         fourier = np.fft.fft2(image)
         fourier_translated = np.fft.fftshift(fourier)
@@ -101,7 +131,7 @@ class ImageFilter:
         kernel = np.dot(linear_gaussian, linear_gaussian.T)
         fourier_translated = fourier_translated * kernel
 
-        # Volta para a imagem
+        # Volta do espectro para a imagem
         fourier_untranslated = np.fft.ifftshift(fourier_translated)
         new_image = np.fft.ifft2(fourier_untranslated)
         new_image = np.abs(new_image)
--- projeto1/image_manager.py
+++ projeto1/image_manager.py
@@ -7,36 +7,56 @@ from image_filter import ImageFilter
 class ImageManager:
 
     @staticmethod
-    def create_filtered_image(filter_type, input_file, output_file='./results', sigma='30'):
+    def create_filtered_image(filter_type, input_file, option, output_file='./results'):
+        """
+        Cria uma imagem nova, aplicando-se um filtro
+        :param filter_type: tipo de filtro. Pode ser h1, h2, h3, h4, h3_h4, g ou  all
+        :param input_file: arquivo de imagem de entrada
+        :param option: opção de cada filtro. Para os filtros h, pode ser - ou binary, indicando que
+                       devem ser transfomrados em imagens binárias. Para o g, indica o desvio padrão da Gaussiana
+        :param output_file: arquivo de saída, onde a imagem deve ser escrita.
+        """
         image = np.float32(cv2.imread(input_file, 0))
-
-        sigma = int(sigma)
 
         if filter_type == 'all':
             ImageManager.__create_all_filtered_images(input_file, output_file, image)
         else:
-            ImageManager.__create_specific_filter(filter_type, image, output_file, sigma)
+            ImageManager.__create_specific_filter(filter_type, image, output_file, option)
 
     @staticmethod
-    def __create_specific_filter(filter_type, image, output_file, sigma):
-        if filter_type == 'h1':
-            image = ImageFilter.filter_h1(image)
-        elif filter_type == 'h2':
-            image = ImageFilter.filter_h2(image)
-        elif filter_type == 'h3':
-            image = ImageFilter.filter_h3(image)
-        elif filter_type == 'h4':
-            image = ImageFilter.filter_h4(image)
-        elif filter_type == 'h3_h4':
-            image = ImageFilter.filter_h3_h4(image)
-        elif filter_type == 'g':
-            image = ImageFilter.filter_gaussian(image, sigma)
+    def __create_specific_filter(filter_type, image, output_file, option):
+        """
+        Trata o caso de ter que criar com um filtro específico e cria o arquivo
+        :param filter_type: Pode ser h1, h2, h3, h4, h3_h4, g ou  all
+        :param image: imagem a ser filtrada
+        :param output_file: arquivo de saída
+        :param option: opção dos filtros
+        """
+        if filter_type == 'g':
+            image = ImageFilter.filter_gaussian(image, int(option))
+        else:
+            binary = option == 'binary'
+            if filter_type == 'h1':
+                image = ImageFilter.filter_h1(image, binary)
+            elif filter_type == 'h2':
+                image = ImageFilter.filter_h2(image, binary)
+            elif filter_type == 'h3':
+                image = ImageFilter.filter_h3(image, binary)
+            elif filter_type == 'h4':
+                image = ImageFilter.filter_h4(image, binary)
+            elif filter_type == 'h3_h4':
+                image = ImageFilter.filter_h3_h4(image, binary)
 
         cv2.imwrite(output_file, image)
 
     @staticmethod
     def __create_all_filtered_images(input_file, out_dir, image):
-
+        """
+        Trata o caso de ter que passar a imagem por todos os filtros
+        :param input_file: caminho do arquivo de entrada
+        :param out_dir: diretório de saída dos resultados
+        :param image: arquivo de imagem
+        """
         # Descobre o nome do arquivo e a extensão
         filename = os.path.basename(input_file)
         filename_parts = os.path.splitext(filename)
@@ -50,7 +70,6 @@ class ImageManager:
         cv2.imwrite(f"{out_dir}/{base}_h4{extension}", ImageFilter.filter_h4(image))
         cv2.imwrite(f"{out_dir}/{base}_h3_h4{extension}", ImageFilter.filter_h3_h4(image))
 
-        # Gera imagens com filtro gaussiano com diferentes sigmas,
-        # de 5 em 5 até 10x menor que o tamanho da imagem
+        # Gera imagens com filtro gaussiano com diferentes desvios padrão
         for sigma in range(5, 51, 5):
             cv2.imwrite(f"{out_dir}/{base}_g_{sigma}{extension}", ImageFilter.filter_gaussian(image, sigma))
