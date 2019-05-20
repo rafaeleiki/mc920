c560d426b452be03951ad33d4cc4b4935221feaa
Rafael Eiki <rafaeleiki@yahoo.com.br>
2019-05-20 16:39:37 -0300

    Working with the lines division

--- projeto 3/main.py
+++ projeto 3/main.py
@@ -13,7 +13,11 @@ def main():
     """
     """
     m = Morphological()
-    m.analyze_text_image('../pictures/text.pbm', './results/text_7.pbm')
+    m.analyze_text_image('./pictures/text.pbm', './results/text_7.pbm')
+    m.analyze_text_image('./pictures/letter_a.ascii.pbm', './results/a_7.pbm')
+    m.analyze_text_image('./pictures/fool.ascii.pbm', './results/asc_7.pbm')
+    m.analyze_text_image('./pictures/math_emporium.pbm', './results/math_7.pbm')
+    m.analyze_text_image('./pictures/scs.ascii.pbm', './results/scs_7.pbm')
 
 
 if __name__ == '__main__':
--- projeto 3/morphological.py
+++ projeto 3/morphological.py
@@ -14,6 +14,7 @@ class ConnectedComponent:
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
+        self.area = (x2 - x1) * (y2 - y1)
         self.black_pixel = None
         self.black_pixel_proportion = None
         self.vertical_transitions = None
@@ -25,9 +26,7 @@ class ConnectedComponent:
 
     def calc_pixels_proportions(self):
         self.black_pixel = np.sum(self.image[self.y1:self.y2, self.x1:self.x2])
-        dx = self.x2 - self.x1
-        dy = self.y2 - self.y1
-        self.black_pixel_proportion = self.black_pixel / (dx * dy)
+        self.black_pixel_proportion = self.black_pixel / self.area
 
     def calc_pixels_transitions(self):
         self.vertical_transitions = 0.0
@@ -43,10 +42,10 @@ class ConnectedComponent:
                 if self.image[row, col] == WHITE and self.image[row, col + 1] == BLACK:
                     self.horizontal_transitions += 1
 
-        self.vertical_proportions = self.vertical_transitions / self.black_pixel_proportion
-        self.horizontal_proportions = self.horizontal_transitions / self.black_pixel_proportion
+        self.vertical_proportions = self.vertical_transitions / self.black_pixel
+        self.horizontal_proportions = self.horizontal_transitions / self.black_pixel
 
-    def draw_rectangle(self):
+    def draw_component_rectangle(self):
         for col in range(self.x1, self.x2):
             self.image[self.y1, col] = BLACK
             self.image[self.y2, col] = BLACK
@@ -54,6 +53,24 @@ class ConnectedComponent:
         for row in range(self.y1, self.y2):
             self.image[row, self.x1] = BLACK
             self.image[row, self.x2] = BLACK
+
+    def draw_word_rectangles(self):
+        return 1
+
+    def is_text(self):
+        return (
+                (0.2 <= self.black_pixel_proportion <= 0.6
+                 and self.vertical_proportions < 0.8
+                 and self.horizontal_proportions < 0.8)
+                or
+                (self.vertical_proportions > 0.5 and self.horizontal_proportions > 0.5
+                    and self.black_pixel_proportion > 0.12)
+        ) # rule 6
+        # return 0.2 <= self.black_pixel_proportion <= 0.64  # rule 5
+        # return self.black_pixel_proportion >= 0.2  # rule 4
+        # return self.black_pixel_proportion >= 0.2 and self.vertical_proportions > 0.14  # rule 3
+        # return self.area >= 12 and self.vertical_proportions > 0.14  # rule 2
+        # return self.vertical_proportions > 0.14 # rule 1
 
 
 class Morphological:
@@ -86,7 +103,7 @@ class Morphological:
 
         # Passo 6: fechamento
         kernel6 = np.ones((1, 30), np.uint8)
-        result6 = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel6)
+        result6 = cv2.morphologyEx(result5, cv2.MORPH_CLOSE, kernel6)
         result6 = np.invert(result6)
         cv2.imwrite(intermediate_path, result6)
 
@@ -111,14 +128,18 @@ class Morphological:
                 elif line.find("Number of connected components") >= 0:
                     process = True
 
-        # Passo 9
-
-        # Passo 10
+        # Passo 9 e 10
         for component in components:
-            component.draw_rectangle()
+            if component.is_text():
+                component.draw_component_rectangle()
+                print("{}, {} - b={}, a={}".format(component.vertical_proportions, component.horizontal_proportions,
+                                                   component.black_pixel_proportion, component.area))
+            else:
+                pass
 
         normalized_image = 1 - normalized_image
         normalized_image *= 255
-        cv2.imwrite("./results/final.pbm", normalized_image)
+        cv2.imwrite(path.replace(".pbm", "_final.pbm"), normalized_image)
+        print("Finished " + path)
 
-        cv2.waitKey(0)
+        # cv2.waitKey(0)
